0a58822d830a4a8620ca73459bd3f41e5a665de4
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2023-09-26 21:38:44 +0900

    Add the epoch.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![feature(lazy_cell)]
 
+use chrono::{DateTime, TimeZone, Utc};
+use std::sync::LazyLock;
+
 pub mod snow_flake_id;
 pub mod snowflake_error;
 pub mod snowflake_id_generator;
 pub mod timestamp;
+
+pub static THE_EPOCH: LazyLock<DateTime<Utc>> =
+	LazyLock::new(|| Utc::with_ymd_and_hms(&Utc, 2023, 09, 01, 0, 0, 0).unwrap());
